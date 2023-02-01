079163e541a2a44dbb44836f5f75d84381a85c67
enabledornot <102371948+enabledornot@users.noreply.github.com>
2023-02-01 13:33:48 -0500

    added stop command and untested autorestart script

--- bot/cogs/commands.py
+++ bot/cogs/commands.py
@@ -5,6 +5,10 @@ class Commands(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send("Pong!!")
+    @commands.command()
+    async def stop(self, ctx):
+        await ctx.send("Stopping bot")
+        await ctx.bot.close()
 
 async def setup(bot):
     await bot.add_cog(Commands(bot))
