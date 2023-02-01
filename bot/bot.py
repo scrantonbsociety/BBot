cf8e38b8a7be796ca820e24228b84d080502b7e6
enabledornot <102371948+enabledornot@users.noreply.github.com>
2023-02-01 12:08:08 -0500

    added cog loader

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ import discord
 import asyncio
 from discord.ext import commands
 import json
+import os
 token = ""
 prefix = ""
 bot = ""
@@ -12,10 +13,17 @@ def loadConfig():
         token = json.load(f)["token"]
     with open("bot.config","r") as f:
         prefix = json.load(f)["prefix"]
-    # print(token)
-    # print(prefix)
+def findAllCogs(fname):
+    clist = []
+    if os.path.isdir(fname):
+        for path in os.listdir(fname):
+            clist = clist + findAllCogs(fname + "\\" + path)
+        return clist
+    else:
+        return [fname.replace("\\",".")[:-3]]
 async def load():
-    await bot.load_extension("commands")
+    for cog in findAllCogs("cogs"):
+        await bot.load_extension(cog)
 loadConfig()
 intents = discord.Intents.default()
 intents.message_content = True
