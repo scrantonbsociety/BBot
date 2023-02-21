b2c82bc21ddd978efd6bdb206ae2c612213554e1
enabledornot <102371948+enabledornot@users.noreply.github.com>
2023-02-21 18:45:01 -0500

    added automatic init script for databases

--- db/database.py
+++ db/database.py
@@ -4,11 +4,26 @@ from psycopg2 import pool
 import json
 import random
 import uuid
+import os
 class db(psycopg2.pool.AbstractConnectionPool):
     def __init__(self,dbconf):
         self.conf = dbconf
     def connect(self):
         self.pool = psycopg2.pool.SimpleConnectionPool(1,20,host=self.conf["host"], port=self.conf["port"], user=self.conf["user"], password=self.conf["password"], database=self.conf["database"])
+        ps_connection = self.pool.getconn()
+        if ps_connection:
+            for init in os.listdir("db/initsql"):
+                fullpath = "db/initsql/" + init
+                if os.path.isfile(fullpath):
+                    with open(fullpath,"r") as f:
+                        fdata = f.read()
+                        ps_cursor = ps_connection.cursor()
+                        ps_cursor.execute(fdata)
+                        ps_connection.commit()
+                        ps_cursor.close()
+            self.pool.putconn(ps_connection)
+                    
+                
     def __getattr__(self, attr):
         if attr in self.__dict__:
             return getattr(self, attr)
